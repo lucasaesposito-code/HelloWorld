201d5c2b70073d89a70da61bff1dd95a275a69a1
lucasaesposito-code <lucasaesposito@gmail.com>
2025-10-04 22:24:55 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Olá! Eu sou o Python.")
+nome = input("Qual é o seu nome? ")
+print("Prazer em te conhecer, " + nome + "!")
+print("Você vem sempre aqui?")
+resposta1 = input() 
+print("Entendi, " + nome + ", como posso te ajudar hoje?")
+resposta2 = input()
+
